1d2853c6fc0b69386f5bb596676b63b469f249b9
alontzukerman <67857535+alontzukerman@users.noreply.github.com>
2020-07-19 13:49:59 +0300

    Add to list function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 const viewSectionElement = document.querySelector('#view-section');
 const listElement = document.createElement('ul');
-
 viewSectionElement.appendChild(listElement);
+
+const addButtonElement = document.querySelector('#addButton'); // call button element
+addButtonElement.addEventListener('click', () => { // add button click function event
+    const inputElement = document.querySelector('#textInput');
+    const valueInput = inputElement.value;
+    inputElement.value = '';
+    const lineListElement = document.createElement('li');
+    listElement.appendChild(lineListElement);
+    lineListElement.innerHTML = valueInput;
+})
+
