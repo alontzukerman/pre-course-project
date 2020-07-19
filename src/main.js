55336dce6a156e5e8b9f95bec8d26e9ef82d0dbe
alontzukerman <67857535+alontzukerman@users.noreply.github.com>
2020-07-19 15:32:38 +0300

    Create container for each todo

--- src/main.js
+++ src/main.js
@@ -7,8 +7,30 @@ addButtonElement.addEventListener('click', () => { // add button click function 
     const inputElement = document.querySelector('#textInput');
     const valueInput = inputElement.value;
     inputElement.value = '';
-    const lineListElement = document.createElement('li');
-    listElement.appendChild(lineListElement);
-    lineListElement.innerHTML = valueInput;
-})
+    const priorityElement = document.querySelector('#prioritySeclector');
+    const valuePriority = priorityElement.value;
+
+    createItemContainer(valuePriority,valueInput);
+
+});
+
+function createItemContainer (prio,data){
+    const containerDiv = document.createElement("div");
+    const priorityDiv = document.createElement("div");
+    const createdDiv = document.createElement("div");
+    const textDiv = document.createElement("div");
+    containerDiv.appendChild(priorityDiv);
+    containerDiv.appendChild(createdDiv);
+    containerDiv.appendChild(textDiv);
+    listElement.appendChild(containerDiv);
+    containerDiv.setAttribute('class','todoContainer');
+    priorityDiv.setAttribute('class','todoPriority');
+    createdDiv.setAttribute('class','todoCreatedAt');
+    textDiv.setAttribute('class','todoText');
+
+    priorityDiv.innerHTML = prio;
+    createdDiv.innerHTML = new Date();
+    textDiv.innerHTML = data;
+}
+
 
